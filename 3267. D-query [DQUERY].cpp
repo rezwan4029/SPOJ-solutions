844fc63d64582f73c263fc33b741a67ab63c5548
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2014-08-01 05:32:08 +0600

    BIT / Offline
    Find  the number of distinct elements in the range A to B

--- 3267. D-query [DQUERY].cpp
+++ 3267. D-query [DQUERY].cpp
@@ -1,6 +1,7 @@
 /*
     Rezwan_4029 , AUST
 */
+
 #include <bits/stdc++.h>
 
 #define pb push_back
@@ -12,6 +13,7 @@
 #define ff first
 #define ss second
 #define mp make_pair
+#define gc getchar
 #define EPS 1e-10
 #define pi 3.1415926535897932384626433832795
 using namespace std;
@@ -28,6 +30,7 @@ typedef pair<int,int>pii;
 typedef pair<string,int>psi;
 typedef pair<Long,Long>pll;
 typedef pair<double,double>pdd;
+typedef vector<pii> vpii;
 
 #define forab(i, a, b)	for (__typeof (b) i = (a) ; i <= b ; ++i)
 #define rep(i, n)		forab (i, 0, (n) - 1)
@@ -37,83 +40,83 @@ typedef pair<double,double>pdd;
 #define rof(i, n)		rofba (i, 1, n)
 #define forstl(i, s)	for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
 
-#ifdef LOCAL
 #define __(args...) {dbg,args; cerr<<endl;}
 struct debugger{template<typename T> debugger& operator , (const T& v){cerr<<v<<"\t"; return *this; }}dbg;
-#define __1D(a,n) {rep(i,n) { if(i) printf(" ") ; cout << a[i] ; } cout << endl;}
+#define __1D(a,n) rep(i,n) { if(i) printf(" ") ; cout << a[i] ; }
 #define __2D(a,r,c,f) forab(i,f,r-!f){forab(j,f,c-!f){if(j!=f)printf(" ");cout<<a[i][j];}cout<<endl;}
-#endif
+
 template<class A, class B> ostream &operator<<(ostream& o, const pair<A,B>& p){ return o<<"("<<p.ff<<", "<<p.ss<<")";} //Pair print
 template<class T> ostream& operator<<(ostream& o, const vector<T>& v){ o<<"[";forstl(it,v)o<<*it<<", ";return o<<"]";} //Vector print
 template<class T> ostream& operator<<(ostream& o, const set<T>& v){ o<<"[";forstl(it,v)o<<*it<<", ";return o<<"]";} //Set print
+template<class T> inline void MAX(T &a , T b){ if (a < b ) a = b;}
+template<class T> inline void MIN(T &a , T b){ if (a > b ) a = b;}
 
 //Fast Reader
-template<class T>inline bool read(T &x){int c=getchar();int sgn=1;while(~c&&c<'0'||c>'9'){if(c=='-')sgn=-1;c=getchar();}for(x=0;~c&&'0'<=c&&c<='9';c=getchar())x=x*10+c-'0'; x*=sgn; return ~c;}
+template<class T>inline bool read(T &x){int c=gc();int sgn=1;while(~c&&c<'0'||c>'9'){if(c=='-')sgn=-1;c=gc();}for(x=0;~c&&'0'<=c&&c<='9';c=gc())x=x*10+c-'0';x*=sgn;return ~c;}
 
 //int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
 //int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
 //int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
 //int dx[]={2,1,-1,-2,-1,1};int dy[]={0,1,1,0,-1,-1}; //Hexagonal Direction
 
-struct node{
-    int s , e , val , pos ;
-    // for query val should be set to 0
-    node(int _s , int _e , int _x , int _p ) {
-        s = _s , e = _e  , val = _x , pos = _p ;
-    }
-    bool operator < ( const node &p ) const {
-        return e < p.e || ( e == p.e && val > p.val ); // input start > query start
-    }
-    node () {}
+enum{ INPUT = 1 , QUERY };
+
+const int MX = 1e6 + 7 ;
+
+struct node {
+    int st , en , id , typ , val ;
 };
 
-const int MX = 30000 *2 +7 ;
+int N , Q , tree[MX] , Ans[MX] , Seen[MX];
 
-vector<node>data;
-int tree[MX] , Ans[200000+7];
-map<int,int>Last;
-int N ;
-void update(int idx , int val){ // set the value of that index
-    while( idx <= N ){
-        tree[idx] += val;
-        idx += idx & -idx ;
-    }
+bool cmp( node a, node b ) {
+    if ( a.en == b.en ) return a.typ < b.typ ;
+    return a.en < b.en ;
 }
-int query(int idx){
+
+void update(int i , int x ) {
+    for( ; i <= N ; i += ( i & -i ) ) tree[i] += x ;
+}
+
+int S(int i){
     int sum = 0 ;
-    while( idx > 0 ){
-        sum += tree[idx];
-        idx -= idx & -idx;
-    }
-    return sum;
+    for( ; i > 0 ; i -= ( i & -i ) ) sum += tree[i] ;
+    return sum ;
 }
 
-int main() {
-    int q ;
+vector< node > V ;
+int main()
+{
+    FI ;
+
     read(N);
-    ms(tree,0); Last.clear();
     For(i,N) {
-        int x ; read(x);
-        data.pb( node(i,i,x,-1) );
+        node inp ;
+        read(inp.val);
+        inp.st = inp.en = i;
+        inp.typ = INPUT ;
+        V.push_back(inp);
     }
-    read(q);
-    For(i,q) {
-        int l , r ;
-        read(l) , read(r);
-        data.pb( node(l,r,0,i) );
+    read(Q);
+    For(i,Q){
+        node inp ;
+        read(inp.st) , read(inp.en);
+        inp.id = i ;
+        inp.typ = QUERY ;
+        V.push_back(inp);
     }
-    sort( all(data) );
-    forstl(i,data){
-        node curr = *i;
-        if( curr.pos == -1 ){
-            if(  Last[ curr.val ] ) update( Last[ curr.val ] ,-1 );
-            update(curr.s,1);
-            Last[ curr.val ] = curr.s;
+    sort( all(V) , cmp );
+    int SZ = V.size() ;
+    rep(i,SZ){
+        if( V[i].typ == INPUT ){
+            if( Seen[ V[i].val ] ){
+                update( Seen[ V[i].val ] , -1 );
+            }
+            Seen[ V[i].val ] = V[i].st ;
+            update( V[i].st , 1);
+        }else{
+            Ans[ V[i].id ] = S( V[i].en ) - S( V[i].st - 1 );
         }
-        else {
-            Ans[ curr.pos ] = query(curr.e) - query(curr.s-1);
-        }
     }
-    For(i,q) printf("%d\n",Ans[i]);
+    For(i,Q) printf("%d\n", Ans[i]);
 }
-
